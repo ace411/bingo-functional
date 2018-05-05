3d8211fceb12c588aa6f477b44ea8a5b5f63d78a
ace411 <lochbm@live.com>
2018-05-05 22:59:18 +0300

    Created fill helper function

---
+++ src/Algorithms/Fill.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Chemem\Bingo\Functional\Algorithms\fill;
+
+const fill = 'Chemem\\Bingo\\Functional\\Algorithms\\fill';
+
+function fill(array $collection, $value, int $start, int $end) : array
+{
+    $vals = array_values($collection);
+    $valCount = count($collection);
+
+    $fillFunc = function (int $init, array $acc) use ($vals, $value, $end, &$fillFunc) {
+        if ($init >= $end) {
+            return $acc;
+        }
+
+        $acc[$init] = $value;
+
+        return $fillFunc($init + 1, $acc);
+    }; 
+
+    return $fillFunc($start, $vals);
+}
