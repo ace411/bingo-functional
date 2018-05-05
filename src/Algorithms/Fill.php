48d1f4d8e49c6d1d7f06fe64eaa75f835c4a6c00
ace411 <lochbm@live.com>
2018-05-06 02:25:16 +0300

    Modified namespace and fill function logic

--- src/Algorithms/Fill.php
+++ src/Algorithms/Fill.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Chemem\Bingo\Functional\Algorithms\fill;
+namespace Chemem\Bingo\Functional\Algorithms;
 
 const fill = 'Chemem\\Bingo\\Functional\\Algorithms\\fill';
 
@@ -10,7 +10,7 @@ function fill(array $collection, $value, int $start, int $end) : array
     $valCount = count($collection);
 
     $fillFunc = function (int $init, array $acc) use ($vals, $value, $end, &$fillFunc) {
-        if ($init >= $end) {
+        if ($init > $end) {
             return $acc;
         }
 
