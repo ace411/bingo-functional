1007140915dfebe3bcee54a69bda54e8208d10d3
ace411 <lochbm@live.com>
2018-06-29 22:14:32 +0300

    Created trampoline function script

---
+++ src/Algorithms/Trampoline.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Chemem\Bingo\Functional\Algorithms;
+
+const trampoline = 'Chemem\\Bingo\\Functional\\Algorithms\\trampoline';
+
+function trampoline(callable $func)
+{
+    $finalArgs = [];
+    $recursing = false;
+
+    return function (...$args) use ($func, $finalArgs, $recursing) {
+        $finalArgs[] = $args;
+
+        if (!$recursing) {
+            $recursing = true;
+
+            while (!empty($finalArgs)) { $result = $func(...array_shift($finalArgs)); }
+
+            $recursing = false;
+        }
+
+        return $result;
+    };
+}
