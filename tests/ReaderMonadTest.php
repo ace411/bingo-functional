f66a68b023c342816d11bc2a3875db91ba37dca7
ace411 <lochbm@live.com>
2018-07-14 12:53:29 +0300

    Created separate Reader monad test script

---
+++ tests/ReaderMonadTest.php
@@ -0,0 +1,120 @@
+<?php
+
+use Chemem\Bingo\Functional\Functors\Monads\Reader;
+use function Chemem\Bingo\Functional\Algorithms\{fold, concat};
+
+class ReaderMonadTest extends \PHPUnit\Framework\TestCase
+{
+    public function testOfStaticMethodOutputsReaderInstance()
+    {
+        $this->assertInstanceOf(Reader::class, Reader::of(function ($name) { return 'Hello ' . $name; }));
+    }
+
+    public function testReaderMonadImplementsFantasyLandMonadInterface()
+    {
+        $interfaces = class_implements(Reader::of(function ($name) { return 'Hello ' + $name; }));
+
+        $this->assertContains(
+            'FunctionalPHP\\FantasyLand\\Monad',
+            $interfaces
+        );
+    }
+
+    public function testApMethodOuputsReaderInstance()
+    {
+        $read = Reader::of(function ($name) { return 'Hello ' . $name; })
+            ->ap(Reader::of(function ($content) { return 'Hi, ' . $content; }));
+
+        $this->assertInstanceOf(Reader::class, $read);
+    }
+
+    public function testApMethodBuildsOnInitialReaderTransformation()
+    {
+        $read = Reader::of(function ($name) { return 'Hello ' . $name; })
+            ->ap(Reader::of(function ($content) { return 'Hi, ' . $content; }))
+            ->run('loki');
+
+        $this->assertEquals('Hi, Hello loki', $read);
+        $this->assertInternalType('string', $read);
+    }
+
+    public function testWithReaderUsesCallbackToTransformInitialReaderValue()
+    {
+        $read = Reader::of(function ($name) { return concat(' ', 'Hello', $name . '.'); })
+            ->withReader(
+                function ($content) { 
+                    return Reader::of(
+                        function ($name) use ($content) { return concat(' ', $content, ($name == 'Loki' ? '' : 'How are you?')); }
+                    ); 
+                }
+            );
+
+        $this->assertEquals('Hello Loki. ', $read->run('Loki'));
+        $this->assertEquals('Hello Michael. How are you?', $read->run('Michael'));
+    }
+
+    public function testWithReaderOutputsReaderInstance()
+    {
+        $read = Reader::of(function ($name) { return concat(' ', 'Hello', $name . '.'); })
+            ->withReader(
+                function ($content) { 
+                    return Reader::of(
+                        function ($name) use ($content) { return concat(' ', $content, ($name == 'Loki' ? '' : 'How are you?')); }
+                    ); 
+                }
+            );
+        
+        $this->assertInstanceOf(Reader::class, $read);
+    }
+
+    public function testMapMethodOutputsReaderInstance()
+    {
+        $read = Reader::of(function ($val) { return $val * 2; })
+            ->map(function ($mult) { return Reader::of(function ($val) use ($mult) { return $mult * ($val + 5); }); });
+
+        $this->assertInstanceOf(Reader::class, $read);
+    }
+
+    public function testMapMethodUsesCallbackToTransformInitialReaderValue()
+    {
+        $read = Reader::of(function ($val) { return $val * 2; })
+            ->map(function ($mult) { return Reader::of(function ($val) use ($mult) { return $mult * ($val + 5); }); })
+            ->run(4);
+
+        $this->assertEquals(72, $read);
+    }
+
+    public function testBindMethodOutputsReaderInstance()
+    {
+        $read = Reader::of(function ($nums) { return fold(function ($acc, $num) { return $acc + $num; }, $nums, 0); })
+            ->bind(
+                function ($sum) { 
+                    return Reader::of(function ($nums) use ($sum) { return fold(function ($acc, $val) { return $acc * $val; }, $nums, $sum); }); 
+                }
+            );
+
+        $this->assertInstanceOf(Reader::class, $read);
+    }
+
+    public function testBindMethodUsesCallbackToTransformInitialReaderValue()
+    {
+        $read = Reader::of(function ($nums) { return fold(function ($acc, $num) { return $acc + $num; }, $nums, 0); })
+            ->bind(
+                function ($sum) { 
+                    return Reader::of(function ($nums) use ($sum) { return fold(function ($acc, $val) { return $acc * $val; }, $nums, $sum); }); 
+                }
+            )
+            ->run(range(1, 3));
+
+        $this->assertInternalType('integer', $read);
+        $this->assertEquals(36, $read);
+    }
+
+    public function testAskMethodOutputsClosure()
+    {
+        $read = Reader::of(function ($name) { return concat(' ', 'Hello', $name); })
+            ->ask();
+
+        $this->assertInstanceOf(\Closure::class, $read);
+    }
+}
