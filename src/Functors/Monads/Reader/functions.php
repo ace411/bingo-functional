520b25035d1b2ed615c6d3f17de63353a4678434
ace411 <lochbm@live.com>
2018-09-15 16:33:08 +0300

    Created Reader monad helper functions script

---
+++ src/Functors/Monads/Reader/functions.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Chemem\Bingo\Functional\Functors\Monads\Reader;
+
+use \Chemem\Bingo\Functional\Functors\Monads\Reader as ReaderMonad;
+
+/**
+ * _return :: Callable value -> Reader r a
+ */
+
+const _return = 'Chemem\\Bingo\\Functional\\Functors\\Monads\\Reader\\_return';
+
+function _return($value) : ReaderMonad
+{
+    return ReaderMonad::of($value);
+}
+
+/**
+ * runReader :: Reader r a -> a
+ */
+
+const runReader = 'Chemem\\Bingo\\Functional\\Functors\\Monads\\Reader\\runReader';
+
+function runReader(ReaderMonad $reader, $value)
+{
+    return $reader->run($value);
+}
+
+/**
+ * mapReader :: (a -> b) -> Reader r a -> Reader r b
+ */
+
+const mapReader = 'Chemem\\Bingo\\Functional\\Functors\\Monads\\Reader\\mapReader';
+
+function mapReader(callable $function, ReaderMonad $reader, $value) : ReaderMonad
+{
+    return _return($function($reader->run($value)));
+}
+
+/**
+ * withReader :: (r -> r') -> Reader r a -> Reader r' a
+ */
+
+const withReader = 'Chemem\\Bingo\\Functional\\Functors\\Monads\\Reader\\withReader';
+
+function withReader(callable $function, ReaderMonad $reader) : ReaderMonad
+{
+    return $reader->withReader($function);
+}
