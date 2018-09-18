d680b998c065ff0c4b5e13eb7845c32f0d70c77b
ace411 <lochbm@live.com>
2018-09-18 14:45:11 +0300

    Fixed namespace error

--- src/Functors/Applicatives/Applicative/functions.php
+++ src/Functors/Applicatives/Applicative/functions.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace Chemem\Bingo\Functional\Applicatives\Applicative;
+namespace Chemem\Bingo\Functional\Functors\Applicatives\Applicative;
 
-use \Chemem\Bingo\Functional\Applicatives\Applicative as App;
+use \Chemem\Bingo\Functional\Functors\Applicatives\Applicative as App;
 
 /**
  * pure :: a -> f a
  */
 
-const pure = 'Chemem\\Bingo\\Functional\\Applicatives\\Applicative\\pure';
+const pure = 'Chemem\\Bingo\\Functional\\Functors\\Applicatives\\Applicative\\pure';
 
 function pure($value) : App
 {
@@ -19,7 +19,7 @@ function pure($value) : App
  * liftA2 :: (a -> b -> c) -> f a -> f b -> f c 
  */
 
-const liftA2 = 'Chemem\\Bingo\\Functional\\Applicatives\\Applicative\\liftA2';
+const liftA2 = 'Chemem\\Bingo\\Functional\\Functors\\Applicatives\\Applicative\\liftA2';
 
 function liftA2(callable $function, App ...$values) : App
 {
