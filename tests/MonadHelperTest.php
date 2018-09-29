b6f50b77efeba1b8299c3d53fa9ad2b24a1a25a9
ace411 <lochbm@live.com>
2018-09-29 06:52:31 +0300

    Created monadic function helper test script

---
+++ tests/MonadHelperTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Chemem\Bingo\Functional\Functors\Monads\IO;
+use function \Chemem\Bingo\Functional\Algorithms\concat;
+use function \Chemem\Bingo\Functional\Functors\Monads\{mcompose, bind};
+
+class MonadHelperTest extends \PHPUnit\Framework\TestCase
+{
+    public function testMComposeFunctionComposesTwoMonadicFunctionsFromRightToLeft()
+    {
+        $res = mcompose(function ($contents) {
+            return IO\IO(strtolower($contents));
+        }, IO\readFile)(IO\IO(concat('/', dirname(__DIR__), 'io.test.txt')));
+
+        $this->assertInstanceOf(IO::class, $res);
+        $this->assertInternalType('string', $res->exec());
+        $this->assertEquals('this is an io monad test file.', $res->exec());
+    }
+
+    public function testBindFunctionSequentiallyComposesTwoActions()
+    {
+        $res = bind(function ($val) {
+            return IO\IO($val * 2);
+        }, IO\IO(2));
+
+        $this->assertInstanceOf(IO::class, $res);
+        $this->assertEquals(4, $res->exec());
+        $this->assertInternalType('integer', $res->exec());
+    }
+}
