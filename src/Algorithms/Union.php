9267b324dfd22706dd310a0fbf43fbf332df1ad5
ace411 <lochbm@live.com>
2018-12-02 01:34:04 +0300

    Modified union function

--- src/Algorithms/Union.php
+++ src/Algorithms/Union.php
@@ -1,5 +1,22 @@
 <?php
 
 /**
+ * 
+ * union function
+ * 
+ * union :: [a] -> [b] -> [a, b]
+ * 
  * @see https://lodash.com/docs/4.17.11#union
+ * @author Lochemem Bruno Michael
+ * @license Apache-2.0
  */
+
+namespace Chemem\Bingo\Functional\Algorithms;
+
+const union = 'Chemem\\Bingo\\Functional\\Algorithms\\union';
+
+function union(array ...$values) : array
+{
+    $res = compose(flatten, unique);
+    return $res($values);
+}
