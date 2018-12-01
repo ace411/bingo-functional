231f0b9c3aeed4c678d24b029cfcfc12998d0c3d
ace411 <lochbm@live.com>
2018-12-02 01:32:34 +0300

    Created zipWith function

---
+++ src/Algorithms/ZipWith.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * 
+ * zipWith function
+ * 
+ * zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+ * 
+ * @see https://lodash.com/docs/4.17.11#zipWith
+ * @author Lochemem Bruno Michael
+ * @license Apache-2.0
+ */
+
+namespace Chemem\Bingo\Functional\Algorithms;
+
+const zipWith = 'Chemem\\Bingo\\Functional\\Algorithms\\zipWith';
+
+function zipWith(callable $function, array ...$values) : array
+{
+    $zipped = zip(...$values);
+
+    return map(function ($zip) use ($function) {
+        return $function(...$zip);
+    }, $zipped);
+}
