aba385bb9dca56b6bcda0cf3dccb053948f1bd98
ace411 <lochbm@live.com>
2018-12-02 17:06:16 +0300

    Updated unionWith function

--- src/Algorithms/UnionWith.php
+++ src/Algorithms/UnionWith.php
@@ -1,5 +1,25 @@
 <?php
 
 /**
+ * 
+ * unionWith function
+ * 
+ * unionWith :: (a -> b -> Bool) -> [a] -> [b] -> [a, b]
+ * 
  * @see https://lodash.com/docs/4.17.11#unionWith
+ * @author Lochemem Bruno Michael
+ * @license Apache-2.0
  */
+
+namespace Chemem\Bingo\Functional\Algorithms;
+
+const unionWith = 'Chemem\\Bingo\\Functional\\Algorithms\\unionWith';
+
+function unionWith(callable $function, array ...$values) : array
+{
+    $acc = [];
+    if ($function(...$values)) {
+        $acc = union(...$values);
+    }
+    return $acc;
+}
