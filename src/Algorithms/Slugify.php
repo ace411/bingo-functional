b0de274108536a422535e08d405fdac4734a3529
ace411 <lochbm@live.com>
2019-02-09 21:36:45 +0300

    Added new functions

---
+++ src/Algorithms/Slugify.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * 
+ * slugify function
+ * 
+ * slugify :: String -> String
+ * 
+ * @author Lochemem Bruno Michael
+ * @license Apache-2.0
+ */
+
+namespace Chemem\Bingo\Functional\Algorithms;
+
+const slugify = 'Chemem\\Bingo\\Functional\\Algorithms\\slugify';
+
+function slugify(string $string) : string
+{
+    $slugify = compose(partial('explode', ' '), function (array $words) {
+        return concat('-', ...$words);
+    });
+
+    return $slugify($string);
+}
---
+++ src/Algorithms/Truncate.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * 
+ * truncate function
+ * 
+ * truncate :: String -> Int -> String
+ * 
+ * @see https://lodash.com/docs/4.17.11#truncate
+ * @author Lochemem Bruno Michael
+ * @license Apache-2.0
+ */
+
+namespace Chemem\Bingo\Functional\Algorithms;
+
+const truncate = 'Chemem\\Bingo\\Functional\\Algorithms\\truncate';
+
+function truncate(string $string, int $limit) : string
+{
+    $strlen = 0;
+    if (!extension_loaded('ext-mbstring')) {
+        $strlen += strlen($string);
+    }
+    $strlen += mb_strlen($string, 'utf-8');
+
+    if ($limit > $strlen) {
+        return $string;
+    }
+
+    $truncate = compose(
+        partialRight('substr', $limit, 0), 
+        partialRight(partial(concat, '..'), '.')
+    );
+
+    return $truncate($string);
+}
