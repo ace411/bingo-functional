397349999f631445792eb79a08b02c34338c16fd
ace411 <lochbm@live.com>
2019-11-29 20:20:03 +0300

    Added Transient mutation test

--- src/Common/Traits/TransientMutator.php
+++ src/Common/Traits/TransientMutator.php
@@ -2,7 +2,10 @@
 
 /**
  * TransientMutator trait.
- *
+ * Adapted from an article written by Edd Man
+ * 
+ * @see https://tech.mybuilder.com/designing-immutable-concepts-with-transient-mutation-in-php/
+ * 
  * @author Lochemem Bruno Michael
  * @license Apache 2.0
  */
