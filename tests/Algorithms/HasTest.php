1ed748764d7911e53b2e38ba843134c24a6f1748
ace411 <lochbm@live.com>
2020-12-22 20:00:30 +0300

    Added has() function

---
+++ tests/Algorithms/HasTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Chemem\Bingo\Functional\Tests\Algorithms;
+
+use Chemem\Bingo\Functional\Algorithms as f;
+
+class HasTest extends \PHPUnit\Framework\TestCase
+{
+  public function contextProvider()
+  {
+    return [
+      [
+        [
+          (object) [(object) \range(1, 9), 'foo', 'bar'],
+          6,
+        ],
+        true,
+      ],
+      [
+        [
+          ['foo', 'bar', 'baz'],
+          'foo-bar',
+        ],
+        false,
+      ],
+    ];
+  }
+
+  /**
+   * @dataProvider contextProvider
+   */
+  public function testHasChecksIfListContainsItem($args, $res)
+  {
+    $check = f\has(...$args);
+
+    $this->assertEquals($res, $check);
+  }
+}
