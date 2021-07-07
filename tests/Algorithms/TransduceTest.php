68227b0d2c02ed0013559a4ebba493ab2cb2dd18
ace411 <lochbm@live.com>
2021-07-08 00:24:08 +0300

    fix: added transduce functions

---
+++ tests/Algorithms/TransduceTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Chemem\Bingo\Functional\Tests\Algorithms;
+
+use Chemem\Bingo\Functional\{
+  Algorithms as f,
+  Algorithms\Transducer as t,
+};
+
+class TransduceTest extends \PHPUnit\Framework\TestCase
+{
+  public function transduceProvider()
+  {
+    return [
+      [
+        f\flip(f\compose)(
+          t\map(function (int $x) {
+            return $x + 3;
+          }),
+          t\map(function (int $x) {
+            return $x ** 2;
+          }),
+          t\filter(function (int $x) {
+            return $x % 2 === 0;
+          })
+        ),
+        function ($acc, $val, $idx) {
+          return f\extend($acc, [$idx => $val]);
+        },
+        [
+          'foo' => 1,
+          'bar' => 3,
+          'baz' => 4,
+        ],
+        [],
+        ['foo' => 16, 'bar' => 36],
+      ],
+      [
+        f\flip(f\compose)(
+          t\map('strtoupper'),
+          t\reject(function (string $str) {
+            return \mb_strlen($str, 'utf-8') > 3;
+          }),
+          t\map(function (string $str) {
+            return f\concat('-', ...\str_split($str));
+          })
+        ),
+        function ($acc, $val, $idx) {
+          return f\extend($acc, [$idx => $val]);
+        },
+        ['foo', 'bar', 'baz', 'foo-bar'],
+        [],
+        ['F-O-O', 'B-A-R', 'B-A-Z'],
+      ],
+    ];
+  }
+
+  /**
+   * @dataProvider transduceProvider
+   */
+  public function testtransduceEffectivelyPipelinesListOperations($transducer, $step, $list, $acc, $res)
+  {
+    $transduced = f\transduce($transducer, $step, $list, $acc);
+
+    $this->assertEquals($res, $transduced);
+  }
+}
