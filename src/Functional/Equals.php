12df617d7077c5657c06539abd0ab3e30ca41b34
ace411 <lochbm@gmail.com>
2023-03-05 13:18:53 +0300

    Added equals() function

---
+++ src/Functional/Equals.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * equals function
+ *
+ * @package bingo-functional
+ * @author Lochemem Bruno Michael
+ * @license Apache-2.0
+ */
+
+namespace Chemem\Bingo\Functional;
+
+use function Chemem\Bingo\Functional\Internal\_refobj;
+
+const equals = __NAMESPACE__ . '\\equals';
+
+/**
+ * equals
+ * perform artifact comparisons to ascertain equivalence
+ *
+ * equals :: a -> b -> Bool -> Bool
+ *
+ * @param mixed $fst
+ * @param mixed $snd
+ * @param boolean $recurse
+ * @return boolean
+ * @example
+ *
+ * equals((object)range(1, 5), (object)range(2, 4));
+ * => false
+ */
+function equals($fst, $snd, bool $recurse = false): bool
+{
+  $ft   = \gettype($fst);
+  $st   = \gettype($snd);
+  // store core types in list to preempt redeclaration
+  $core = [
+    'integer',
+    'string',
+    'boolean',
+    'array',
+    'double',
+    'NULL',
+  ];
+
+  if ($ft !== $st) {
+    return false;
+  }
+
+  if (\in_array($ft, $core) && \in_array($st, $core)) {
+    return $fst === $snd;
+  }
+
+  if ($ft === 'resource' && $st === 'resource') {
+    return \get_resource_type($fst) === \get_resource_type($snd);
+  }
+
+  if ($ft === 'object' && $st === 'object') {
+    return _refobj($fst, $recurse) === _refobj($snd, $recurse);
+  }
+
+  return false;
+}
---
+++ tests/Functional/EqualsTest.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Chemem\Bingo\Functional\Tests\Functional;
+
+use Chemem\Bingo\Functional as f;
+use Chemem\Bingo\Functional\Functors\Monads as m;
+
+class EqualsTest extends \PHPUnit\Framework\TestCase
+{
+  private static $file;
+  private static $curl;
+
+  public function contextProvider()
+  {
+    return [
+      // array comparisons
+      [
+        [
+          ['bingo-functional'],
+          ['functional'],
+        ],
+        false
+      ],
+      [
+        [
+          ['foo' => 'foo'],
+          ['bar' => 'bar'],
+        ],
+        false,
+      ],
+      [
+        [
+          ['functional'],
+          ['functional'],
+        ],
+        true,
+      ],
+      [
+        [
+          ['foo' => 'foo', 'bar' => 'bar'],
+          ['foo' => 'foo', 'bar' => 'bar'],
+        ],
+        true,
+      ],
+      // string comparisons
+      [['chemem', 'mike'], false],
+      [['mike', 'mike'], true],
+      // integer comparisons
+      [[12, 4], false],
+      [[3, 3], true],
+      // float/double comparisons
+      [[12.244, 12.24], false],
+      [[2.22, 2.22], true],
+      // null comparisons
+      [[null, null], true],
+      // cross comparisons
+      [
+        [null, \range(1, 3)],
+        false,
+      ],
+      [
+        ['foo', 11.32],
+        false,
+      ],
+      // object comparisons
+      [
+        [(object)\range(1, 3), (object)\range(2, 3)],
+        false,
+      ],
+      [
+        [
+          m\Maybe::fromValue(12),
+          m\Maybe::fromValue(
+            function ($x) {
+              return $x + 1;
+            }
+          ),
+        ],
+        false,
+      ],
+      [
+        [
+          m\Maybe::fromValue((object)\range(1, 3)),
+          m\Maybe::fromValue((object)\range(1, 3)),
+        ],
+        false,
+      ],
+      [
+        [
+          function ($x, $y) {
+            return $x + $y;
+          },
+          function ($x, $y) {
+            return $x - $y;
+          },
+        ],
+        false,
+      ],
+      [
+        [
+          function ($x, $y) {
+            static $val = 2;
+            return $val + $x + $y;
+          },
+          function ($x, $y) {
+            return $x + $y + 2;
+          },
+        ],
+        false,
+      ],
+      [
+        [f\map, f\filter],
+        false,
+      ],
+      [
+        [f\map, f\map],
+        true,
+      ],
+      [
+        [(object)\range(1, 3), (object)\range(1, 3)],
+        true,
+      ],
+      [
+        // recursively traverse object props
+        [
+          m\Maybe::fromValue((object)\range(1, 3)),
+          m\Maybe::fromValue((object)\range(1, 3)),
+          true,
+        ],
+        true,
+      ],
+    ];
+  }
+
+  /**
+   * @dataProvider contextProvider
+   */
+  public function testequalsPerformsArtifactComparisonsToAscertainEquivalence($args, $result)
+  {
+    $comp = f\equals(...$args);
+
+    $this->assertIsBool($comp);
+    $this->assertEquals($result, $comp);
+  }
+
+  public function setUp(): void
+  {
+    self::$file = \fopen(f\filePath(0, 'io.test.txt'), 'r');
+    self::$curl = \curl_init();
+  }
+
+  public function testequalsCanPerformResourceComparisonsToAscertainEquivalence()
+  {
+    $this->assertNotEquals(true, f\equals(self::$file, self::$curl));
+    $this->assertEquals(true, f\equals(self::$file, self::$file));
+    $this->assertEquals(true, f\equals(self::$curl, self::$curl));
+  }
+
+  public function tearDown(): void
+  {
+    \fclose(self::$file);
+    \curl_close(self::$curl);
+  }
+}
