80e1156bb825e9726d4ff356b6dd6f23153a81c6
agiroloki <lochbm@live.com>
2026-01-28 23:55:33 +0300

    Fixed errant tests

--- tests/Functional/AssocPathTest.php
+++ tests/Functional/AssocPathTest.php
@@ -19,13 +19,52 @@ class AssocPathTest extends \PHPUnit\Framework\TestCase
         (object) ['foo' => (object) ['bar' => ['baz' => 3]]],
         ['foo', 'bar', 'baz'],
         'baz',
-        (object) ['foo' => (object) ['bar' => ['baz' => 'baz']]],
+        [
+          'foo'     => [
+            'bar'   => [
+              'baz' => 'baz',
+            ],
+          ],
+        ],
       ],
       [
         [],
         'foo.bar[1]',
         'bar',
         ['foo' => ['bar' => [1 => 'bar']]],
+      ],
+      [
+        (object) [
+          'foo' => 12,
+          'bar' => (object) \range(1, 3),
+        ],
+        'foo.bar.qux',
+        'quux',
+        [
+          'foo'     => [
+            'bar'   => [
+              'qux' => 'quux',
+            ],
+          ],
+          'bar'     => (object) \range(1, 3),
+        ],
+      ],
+      [
+        new class () {
+          public $foo = 12;
+          public $bar = [1, 2, 3];
+        },
+        'qux.quux',
+        ['foo' => 'foo'],
+        [
+          'foo'     => 12,
+          'bar'     => [1, 2, 3],
+          'qux'     => [
+            'quux'  => [
+              'foo' => 'foo',
+            ],
+          ],
+        ],
       ],
     ];
   }
